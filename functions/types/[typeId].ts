5db12e136b39a8a5126fbcd731b5d55630be5b35
rikut0-otsu <107250013+rikut0-otsu@users.noreply.github.com>
2026-03-20 02:13:41 +0900

    tmp

--- functions/types/[typeId].ts
+++ functions/types/[typeId].ts
@@ -1,4 +1,4 @@
-import { renderPageWithMetadata, type OGPContext } from "./_lib/og";
+import { renderPageWithMetadata, type OGPContext } from "../_lib/og";
 
 export async function onRequestGet(context: OGPContext) {
   return renderPageWithMetadata(context);
