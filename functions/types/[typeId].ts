9469034602f4aae695b3f86802213fb39bddd32a
rikut0-otsu <107250013+rikut0-otsu@users.noreply.github.com>
2026-03-20 03:37:03 +0900

    fix

--- functions/types/[typeId].ts
+++
@@ -1,9 +0,0 @@
-import { renderTypePageWithMetadata, type OGPContext } from "../_lib/og";
-
-export async function onRequestGet(context: OGPContext) {
-  return renderTypePageWithMetadata(context);
-}
-
-export async function onRequestHead(context: OGPContext) {
-  return renderTypePageWithMetadata(context);
-}
